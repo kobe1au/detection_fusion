016750801bcc1d4681767dde3d53c2488eab3c5c
kobe1au <zliuqingtx@gmail.com>
2026-06-26 15:14:11 +0800

    代码提交31

--- fusion/quality.py
+++ fusion/quality.py
@@ -51,6 +51,11 @@ OBSERVABLE_OPTIONAL_NUMERIC_FIELDS = (
     "api_encoder_coverage",
     "api_truncated_by_encoder_budget",
     "api_integrity_before_encoder_budget",
+
+    # add graph encoder-budget diagnostics
+    "graph_encoder_coverage",
+    "graph_truncated_by_encoder_budget",
+    "graph_integrity_before_encoder_budget",
 )
 
 OBSERVABLE_ERROR_FIELDS = (
